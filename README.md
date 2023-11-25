2804b7bf18bcb3eac16745f8f3df83dd22335fd4
Cristian Ojeda <ojeda.cris.97@gmail.com>
2023-11-24 23:55:45 -0300

    actualizacion de pag

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-El documento esta listo para implementar en la pagina que se desee usar el formulario de contacto
+El formulario esta listo para ser implementado.
+
+Mediante bootstrap se le dio estilos al proyecto. 
+
+Tiene un archivo js donde mediante logica al elegir el plan sale un cartel que indica el monto a pagar.
+
+Luego de dar click en enviar o hacer submit sale un modal indicando que nos pondremos en contacto para dar servicio y/o seguimiento.
+
+El proyecto se levanta mediante la extension Live Server.
--- js/app.js
+++ js/app.js
@@ -6,18 +6,32 @@
 
     switch (opcion) {
       case 'opcion1':
-        precios.innerHTML= `<div class="py-2 rounded-3 border border-3 border-secondary-subtle bg-danger"><h4>El basico tiene un coste de $500.</h4></div>`
+        precios.innerHTML= `
+                <div class="p-3 rounded-3 border border-3 border-secondary-subtle bg-danger">
+                            <h4>El basico tiene un coste de $500.</h4></div>`
         break;
       case 'opcion2':
-        precios.innerHTML= `<div class="py-2 rounded-3 border border-3 border-secondary-subtle bg-danger" ><h4>El intermedio tiene un coste de $1000.</h4></div>`
+        precios.innerHTML= `
+                <div class="p-3 rounded-3 border border-3 border-secondary-subtle bg-danger">
+                            <h4>El intermedio tiene un coste de $1000.</h4></div>`
         break;
       case 'opcion3':
-        precios.innerHTML= `<div class="py-2 rounded-3 border border-3 border-secondary-subtle bg-danger"><h4>El premium tiene un coste de $1500.</h4></div>`
+        precios.innerHTML= `
+                <div class="p-3 rounded-3 border border-3 border-secondary-subtle bg-danger">
+                            <h4>El premium tiene un coste de $1500.</h4></div>`
         break;
         
         default:
-          precios.innerHTML= `<div class="py-2 rounded-3 border border-3 border-secondary-subtle bg-danger"><h4>Elije uno!</h4></div>`
+          precios.innerHTML= `
+                <div class="p-3 rounded-3 border border-3 border-secondary-subtle bg-danger">
+                            <h4>Elije uno!</h4></div>`
         break;
     }
 
+  })
+
+  let btn_form = document.getElementById("btn-form")
+  btn_form.addEventListener("click", (e)=>{
+    e.preventDefault()
+    
   })
